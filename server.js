684837b7f62cf16b9a779e049e9c7f887710d73c
Tania Jebin <shova_khan@yahoo.com>
2020-07-16 18:38:31 -0400

    hello

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
